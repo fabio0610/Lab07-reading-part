ca67e66cf8b21ef38c2bdde7d8f2ce84a2b96093
fabio0610 <fabio0610@hotmail.com>
2020-10-08 23:44:32 -0600

    cambios menores

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,27 @@
 #include <iostream>
+#include <../lib/nlohmann/json.hpp>
+#include <iostream>
+#include <fstream>
 
+using namespace std;
+using json = nlohmann::json;
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+
+    ifstream archivo1;
+    string texto;
+    try { archivo1.open("ArchivoJson.json", ios::binary); }
+    catch (ifstream::failure a) {
+        cout << "no se pudo abrir el archivo";
+        exit(1);
+    }
+    cout << endl;
+    while (!archivo1.eof()) {
+        getline(archivo1, texto);
+        cout << texto << endl;
+    }
+    archivo1.close();
+
+
+
     return 0;
 }
